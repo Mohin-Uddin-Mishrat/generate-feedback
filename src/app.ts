0ef6a5cb10c842ab20e5a0b5902d42475eda93fc
Your Name <mmishrat90@gmail.com>
2026-03-10 17:51:02 +0600

    update-and-fix

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cors({
 
-    origin: ["http://localhost:3000"]
+    origin: ["http://localhost:3000", "*" , "https://generate-feedback.onrender.com"]
 }))
 app.use(express.json({ limit: "100mb" }))
 app.use(express.raw())
